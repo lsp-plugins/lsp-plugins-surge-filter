0ca890337552a7e993d5faad30583522a4ba28c3
Vladimir Sadovnikov <sadko4u@gmail.com>
2021-08-17 01:07:05 +0300

    Updated dependencies

--- project.mk
+++ project.mk
@@ -21,7 +21,7 @@
 # Package version
 ARTIFACT_ID                 = LSP_PLUGINS_SURGE_FILTER
 ARTIFACT_NAME               = lsp-plugins-surge-filter
-ARTIFACT_DESC               = Trigger plugin series
+ARTIFACT_DESC               = Surge Filter plugin series
 ARTIFACT_HEADERS            = lsp-plug.in
 ARTIFACT_EXPORT_ALL         = 1
 ARTIFACT_VERSION            = 1.0.1-devel
