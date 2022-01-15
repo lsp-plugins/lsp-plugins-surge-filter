fcaffa31466046955184f2815ef9d6c44ae47357
sadko4u <sadko4u@gmail.com>
2022-01-16 01:43:59 +0300

    Updated build scripts

--- plugins.mk
+++ plugins.mk
@@ -18,13 +18,6 @@
 # along with lsp-plugins-surge-filter.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-# Package version
-ARTIFACT_ID                 = LSP_PLUGINS_SURGE_FILTER
-ARTIFACT_NAME               = lsp-plugins-surge-filter
-ARTIFACT_TYPE               = plug
-ARTIFACT_DESC               = LSP Surge Filter Plugin Series
-ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.1-devel
-
-
-
+# Variables that describe dependencies
+PLUGIN_DEPENDENCIES     = LSP_PLUGINS_SURGE_FILTER
+PLUGIN_PACKAGE_NAME     = lsp-plugins
