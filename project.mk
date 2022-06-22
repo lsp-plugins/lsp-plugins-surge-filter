afb937e82be171fb2f0d25673f825324f8ea68d6
sadko4u <sadko4u@gmail.com>
2022-06-22 21:58:15 +0300

    Version up

--- project.mk
+++ project.mk
@@ -24,7 +24,7 @@ ARTIFACT_NAME               = lsp-plugins-surge-filter
 ARTIFACT_TYPE               = plug
 ARTIFACT_DESC               = LSP Surge Filter Plugin Series
 ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.3
+ARTIFACT_VERSION            = 1.0.4-devel
 
 
 
